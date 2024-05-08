ac87443c51c17593b4e13222f207b8837fadccbb
2210883511 <2210883511@qq.com>
2024-05-08 14:23:40 +0800

    用户端开发

--- src/test/java/com/zzuli/gaokao/UniversityServiceTests.java
+++ src/test/java/com/zzuli/gaokao/UniversityServiceTests.java
@@ -1,11 +1,6 @@
 package com.zzuli.gaokao;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zzuli.gaokao.service.UniversityService;
-import com.zzuli.gaokao.vo.UniversityVo;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,12 +11,5 @@ public class UniversityServiceTests {
     @Autowired
     private UniversityService universityService;
 
-    @Test
-    public void get() throws JsonProcessingException {
-        Page<UniversityVo> universityVoPage = universityService.selectCustom();
-        ObjectMapper objectMapper = new ObjectMapper();
-        String s = objectMapper.writeValueAsString(universityVoPage);
-        System.out.println(s);
-    }
 
 }
